612650f08dbb2f32120bdf29ab9a751cae9b9167
AnaDragulinescu <AnaDragulinescu@users.noreply.github.com>
2022-06-08 13:58:44 +0300

    p

---
+++ src/variab.h
@@ -0,0 +1,11 @@
+// const char* ssid = "comm.pub.ro1";
+// const char* password = "c4t3dr4c0mm";
+
+const char* ssid_f = "SmartAgro";
+const char* password_f = "SA123456";
+
+// const char* ssid_f = "AMSamsung";
+// const char* password_f = "a17b24c15";
+
+const char* mqtt_server_f = "195.201.227.170";
+// const char* mqtt_server = "141.85.161.86";
